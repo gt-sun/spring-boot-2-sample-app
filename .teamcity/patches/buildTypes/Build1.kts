f0931410edf246354db250999caef3c0dffb240c
a9k47zz <a9k47zz@TeamCity>
2019-06-20 10:36:31 +0800

    TeamCity change in 'carl-test / springboot' project: runners of 'Build' build configuration were updated

--- .teamcity/patches/buildTypes/Build1.kts
+++ .teamcity/patches/buildTypes/Build1.kts
@@ -1,6 +1,7 @@
 package patches.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2018_2.*
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.maven
 import jetbrains.buildServer.configs.kotlin.v2018_2.ui.*
 
 /*
@@ -9,6 +10,18 @@ To apply the patch, change the buildType with id = 'Build1'
 accordingly, and delete the patch script.
 */
 changeBuildType(RelativeId("Build1")) {
+    expectSteps {
+        maven {
+            name = "mvnBuild"
+            goals = "package"
+            mavenVersion = bundled_3_5()
+            userSettingsSelection = "settings.xml"
+        }
+    }
+    steps {
+        items.removeAt(0)
+    }
+
     expectDisabledSettings("RUNNER_6")
     updateDisabledSettings("RUNNER_27", "RUNNER_28", "RUNNER_6")
 }
